e019e732cbc91d600e4ee818953e8c9ec6893d42
George Nassas <gnassas@gmail.com>
2012-02-04 01:13:20 -0500

    Minify css & js

--- app.js
+++ app.js
@@ -9,16 +9,35 @@ var http = require('http');
 var mythtv = require('./mythtv')();
 var url = require('url');
 var gzip = require('connect-gzip');
+var piler = require('piler');
 
 
 // Configuration
 
+var clientjs = piler.createJSManager({ urlRoot : "/min/" });
+var clientcss = piler.createCSSManager({ urlRoot : "/min/" });
+
 app.configure(function(){
+
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+
+    clientjs.bind(app);
+    clientcss.bind(app);
+
+    clientcss.addFile(__dirname + "/public/css/HTML5Reset.css");
+    clientcss.addFile(__dirname + "/public/css/overcast/jquery-ui-1.8.16.custom.css");
+    clientcss.addFile(__dirname + '/public/css/mythnode.css');
+
+    clientjs.addFile(__dirname + "/public/js/jquery-1.6.2.min.js");
+    clientjs.addFile(__dirname + "/public/js/jquery-ui-1.8.16.custom.min.js");
+    clientjs.addFile(__dirname + "/public/js/jquery.history.js");
+    clientjs.addFile(__dirname + "/public/js/jquery.timer.js");
+    clientjs.addFile(__dirname + "/public/js/mythnode.js");
+
 });
 
 app.configure('development', function(){
